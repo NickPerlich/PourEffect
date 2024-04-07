0fa7d13897476fd61363944fb21e69d7624a7dd0
NickPerlich <nicholasperlich2003@gmail.com>
2024-04-07 11:53:30 -0700

    made progress on scripts for water pour

--- Assets/Scripts/PourDetector.cs
+++ Assets/Scripts/PourDetector.cs
@@ -1,7 +1,76 @@
-﻿using System.Collections;
+﻿/*
+ * Copyright (c) 2024 Andrew
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using System.Collections;
 using UnityEngine;
 
 public class PourDetector : MonoBehaviour
 {
+    public int pourThreshold = 45;
+    public Transform origin = null;
+    public GameObject streamPrefab = null;
 
+    private bool isPouring = false;
+    private Stream currentStream = null;
+
+    private void Update()
+    {
+        bool pourCheck = CalculatePourAngle() < pourThreshold;
+
+        if (isPouring != pourCheck)
+        {
+            isPouring = pourCheck;
+
+            if (isPouring)
+            {
+                StartPour();
+            }
+            else
+            {
+                EndPour();
+            }
+        }
+    }
+
+    private void StartPour()
+    {
+        print("Start");
+        currentStream = CreateStream();
+        currentStream.Begin();
+    }
+
+    private void EndPour()
+    {
+        print("End");
+    }
+
+    private float CalculatePourAngle()
+    {
+        return transform.forward.y * Mathf.Rad2Deg;
+    }
+
+    private Stream CreateStream()
+    {
+        GameObject streamObject = Instantiate(streamPrefab, origin.position, Quaternion.identity, transform);
+        return streamObject.GetComponent<Stream>();
+    }
 }
--- Assets/Scripts/Stream.cs
+++ Assets/Scripts/Stream.cs
@@ -1,8 +1,76 @@
-﻿using System.Collections;
+﻿/*
+ * Copyright (c) 2024 Andrew
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Stream : MonoBehaviour
 {
-   
+    private LineRenderer lineRenderer = null;
+    private Vector3 targetPosition = Vector3.zero;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Start()
+    {
+        MoveToPosition(0, transform.position);
+        MoveToPosition(1, transform.position);
+    }
+
+    public void Begin()
+    {
+        StartCoroutine(BeginPour());
+    }
+
+    private IEnumerator BeginPour()
+    {
+        while (gameObject.activeSelf)
+        {
+            targetPosition = FindEndPoint();
+
+            MoveToPosition(0, transform.position);
+            MoveToPosition(1, targetPosition);
+
+            yield return null;
+        }
+    }
+
+    private Vector3 FindEndPoint()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, Vector3.down);
+
+        Physics.Raycast(ray, out hit, 2.0f);
+        Vector3 endPoint = hit.collider ? hit.point : ray.GetPoint(2.0f);
+
+        return endPoint;
+    }
+
+    private void MoveToPosition(int index, Vector3 targetPosition)
+    {
+        lineRenderer.SetPosition(index, targetPosition);
+    }
 }
